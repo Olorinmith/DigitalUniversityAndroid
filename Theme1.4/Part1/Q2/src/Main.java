e6ad1f94de21a348a19a965af89e61faa4a3cc64
Olorinmith <sosnin2001@gmail.com>
2023-11-12 17:51:45 +0300

    q2 done

--- Theme1.4/Part1/Q2/src/Main.java
+++ Theme1.4/Part1/Q2/src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] x = {4, 5, 6, 1, 2, 3};
+
+        System.out.println(findElementIndex(x, 7));
+    }
+
+    public static int findElementIndex(int[] arr, int x){
+        //sort arr
+        for(int i = 1; i < arr.length; ++i){
+            int a = arr[i];
+            int j = i;
+            while (j > 0 && arr[j-1]>a){
+                arr[j] = arr[j-1];
+                --j;
+            }
+            arr[j] = a;
+        }
+        //binary search
+        int low = 0;
+        int high = arr.length;
+        int index = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if(mid >= arr.length){break;}
+            if (arr[mid] < x) {
+                low = mid + 1;
+            } else if (arr[mid] > x) {
+                high = mid - 1;
+            } else if (arr[mid] == x) {
+                index = mid;
+                break;
+            }
+        }
+        return index;
+
     }
 }
