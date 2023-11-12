45a9f526ec572bbe7888f01b2d5610410b10c95f
Olorinmith <sosnin2001@gmail.com>
2023-11-12 16:21:57 +0300

    finished q4

--- Theme1.3/part2/Q4/src/Main.java
+++ Theme1.3/part2/Q4/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(compareDouble(11.111112, 11.111111));
+    }
+    public static String compareDouble(double a, double b){
+        double eps = 0.000001;
+        if(a - b > eps){
+            return "первое число больше";
+        } else if (b - a > eps) {
+            return "второе число больше";
+        } else {
+            return "равны";
+        }
     }
 }
