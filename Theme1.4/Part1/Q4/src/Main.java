671504a297808d8dfcc970950c9cb30f2a61eb6d
Olorinmith <sosnin2001@gmail.com>
2023-11-12 18:04:10 +0300

    q4 done q5 created

--- Theme1.4/Part1/Q4/src/Main.java
+++ Theme1.4/Part1/Q4/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] mtx = inputArray(3);
+        System.out.println(Arrays.toString(mtx));
+    }
+
+    public static int[] inputArray(int n){
+        int[] matrix = new int[n];
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < n; i++){
+            matrix[i] = input.nextInt();
+        }
+        return matrix;
     }
 }
