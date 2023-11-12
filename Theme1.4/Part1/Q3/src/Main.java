6a041e46cefddf27f8314406519d57552bdbf1fd
Olorinmith <sosnin2001@gmail.com>
2023-11-12 17:58:25 +0300

    q3 done

--- Theme1.4/Part1/Q3/src/Main.java
+++ Theme1.4/Part1/Q3/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] mtx = inputMatrix(3, 3);
+        System.out.println(Arrays.deepToString(mtx));
+    }
+
+    public static int[][] inputMatrix(int n, int m){
+        int[][] matrix = new int[n][m];
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                matrix[i][j] = input.nextInt();
+            }
+        }
+        return matrix;
     }
 }
