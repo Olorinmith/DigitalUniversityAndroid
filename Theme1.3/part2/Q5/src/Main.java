d4dfafba9499894f9f711d223ab6cc8f1fda5a1e
Olorinmith <sosnin2001@gmail.com>
2023-11-12 16:37:56 +0300

    Finished theme 1.3

--- Theme1.3/part2/Q5/src/Main.java
+++ Theme1.3/part2/Q5/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPowerOfTwo(3));
+    }
+
+    public static boolean isPowerOfTwo(int n){
+        if(n == 0) {
+            return false;
+        }
+        int copy = n;
+
+        while ((copy & 1) == 0) {
+            copy >>=1;
+        }
+        return copy == 1;
     }
 }
