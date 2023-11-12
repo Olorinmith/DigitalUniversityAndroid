3765df26a237a699b5e4b276bf40e72b6b29718d
Olorinmith <sosnin2001@gmail.com>
2023-11-12 16:08:38 +0300

    finished q2

--- Theme1.3/Theme1.3.Part2.Q2/src/Main.java
+++ Theme1.3/Theme1.3.Part2.Q2/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Main.findMaxDouble(10.0, 11.0, 11.2));
+    }
+    public static double findMaxDouble(double num1, double num2, double num3){
+        if (num1 > num2){
+            if (num1 > num3){
+                return num1;
+            } else {
+                return num3;
+            }
+        } else {
+            if (num2 > num3) {
+                return num2;
+            } else {
+                return num3;
+            }
+        }
     }
 }
