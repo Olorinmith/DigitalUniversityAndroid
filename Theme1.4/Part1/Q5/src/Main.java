9dbc6dfe6f29aa79fe34b5d5ffc3eaedb8c1523a
Olorinmith <sosnin2001@gmail.com>
2023-11-12 18:09:44 +0300

    q5 done, part1 done

--- Theme1.4/Part1/Q5/src/Main.java
+++ Theme1.4/Part1/Q5/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] x = {7, 3, 4, 2, 1};
+        sortArraySlice(x);
+        System.out.println(Arrays.toString(x));
+    }
+
+    public static void sortArraySlice(int[] arr){
+        for(int i = 1; i < arr.length-1; ++i){
+            int a = arr[i];
+            int j = i;
+            while (j > 1 && arr[j-1]>a){
+                arr[j] = arr[j-1];
+                --j;
+            }
+            arr[j] = a;
+        }
     }
 }
